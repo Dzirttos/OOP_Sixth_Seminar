8667e2fe033d4df00a3f0dde8a1d0fbfa8fa296e
Dizrtt <demonic2003@bk.ru>
2023-05-22 23:47:42 +0200

    NotesOperation and NotesOperationImpl  Classes filled.

--- model/NotesOperationImpl.java
+++ model/NotesOperationImpl.java
@@ -1,5 +1,65 @@
 package model;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class NotesOperationImpl implements NotesOperation{
+
+    private String noteBookName;
+
+    public NotesOperationImpl(String noteBookName){
+        this.noteBookName = noteBookName;
+        try (FileWriter writer  = new FileWriter(noteBookName,true)){
+            writer.flush();
+        } catch (IOException exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    @Override
+    public List<String> readAllLines() {
+        List<String> lines = new ArrayList<>();
+        try {
+            File file = new File(noteBookName);
+            FileReader fileReader = new FileReader(file);
+            BufferedReader reader = new BufferedReader(fileReader);
+            String line = reader.readLine();
+            if (line != null){
+                lines.add(line);
+            }
+            while (line != null){
+                line = reader.readLine();
+                if (line != null) lines.add(line);
+            }
+            fileReader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();  
+        }
+        return lines;
+    }
+
+
+    @Override
+    public void saveAllLines(List<String> lines) {
+        try {FileWriter writer = new FileWriter(noteBookName,false);
+            for (String line : lines){
+                writer.write(line);
+                writer.append("\n");
+            }
+            writer.flush();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+
+        }
+    }
     
 }
