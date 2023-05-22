fc38a0107283abea76d4a1d57accc5914418cc32
Dizrtt <demonic2003@bk.ru>
2023-05-23 00:10:51 +0200

    NotesController Class filled.

--- controllers/NotesController.java
+++ controllers/NotesController.java
@@ -1,5 +1,40 @@
 package controllers;
 
+import java.util.List;
+
+import model.NoteBook;
+import model.Notes;
+
 public class NotesController {
-    
+    private final NoteBook noteBook;
+
+    public NotesController(NoteBook noteBook) {
+        this.noteBook = noteBook;
+    }
+
+    public void saveNote(Notes note) {
+        NoteBook.createNote(note);
+    }
+
+    public Notes readNote(String noteId) throws Exception {
+        List<Notes> allNotes = noteBook.getAllNotes();
+        for (Notes note : allNotes) {
+            if (note.getId().equals(noteId)) {
+                return note;
+            }
+        }
+        throw new Exception("\u001B[31mNote is not found!\u001B[37m");
+    }
+
+    public List<Notes> getNotes() {
+        return noteBook.getAllNotes();
+    }
+
+    public void updateNote(Notes updatedNote) {
+        noteBook.createNote(updatedNote);
+    }
+
+    public void deleteNote(String noteId) {
+        noteBook.deleteNote(noteId);
+    }
 }
